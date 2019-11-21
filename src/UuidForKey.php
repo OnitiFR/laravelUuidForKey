435009231b6eaad462f155048e0f466d4e997117
François BOUYER <francois@oniti.fr>
2019-11-21 11:45:15 +0100

    Modification pour avoir accès a l'uuid

--- src/UuidForKey.php
+++ src/UuidForKey.php
@@ -9,7 +9,13 @@ trait UuidForKey
   public static function bootUuidForKey()
    {
        static::creating(function ($model) {
-           $model->{$model->getKeyName()} = (string)($model->getTable().'_'.Uuid::uuid4());
+           $model->{$model->getKeyName()} = $model->getUuid();
        });
+   }
+
+   public function getUuid() : string{
+       if($this->{$this->getKeyName()}) return $this->{$this->getKeyName()};
+
+       return (string)($this->getTable().'_'.Uuid::uuid4());;
    }
 }
